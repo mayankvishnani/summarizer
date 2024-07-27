66d9d59a7ce95a9311acf202b95e34ce06420652
mayankvishnani <121482112+mayankvishnani@users.noreply.github.com>
2024-07-27 14:34:29 +0530

    normalizer removed

--- src/services/store.js
+++ src/services/store.js
@@ -1,7 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { articleApi } from "./article";
-import { getDefaultNormalizer } from "@testing-library/react";
-
 export const store = configureStore({
     reducer: {
         [articleApi.reducerPath] : articleApi.reducer
